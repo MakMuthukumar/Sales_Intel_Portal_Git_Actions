13cef1b8993b78297e8265115f6603e745514c0a
MakMuthukumar <muthumak@gmail.com>
2023-02-01 20:47:20 +0530

    Implented Cucumber Reporting artifacts and Extent Report Artifacts in Git Actions CI/CD

--- src/test/java/org/runner/Runner.java
+++ src/test/java/org/runner/Runner.java
@@ -1,7 +1,12 @@
 package org.runner;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.base.Global;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
@@ -14,6 +19,38 @@ import io.cucumber.testng.CucumberOptions;
 				"junit:src\\test\\resources\\Reports\\XML\\cucumber.xml" })
 public class Runner extends AbstractTestNGCucumberTests {
 
+	@BeforeSuite
+	public void allureReportCleanup() throws Throwable {
+		String allureFile = System.getProperty("user.dir") + "\\allure-results";
+		String cucumberFile = System.getProperty("user.dir") + "\\src\\test\\resources\\Reports\\CucumberReports\\cucumber-html-reports";
+		File allureFileDirectoy = new File(allureFile);
+
+		if (allureFileDirectoy.isDirectory()) {
+			try {
+				FileUtils.cleanDirectory(allureFileDirectoy);
+				Thread.sleep(2000);
+				System.out.println("Previously generated Allure Execution Files Deleted Permanently");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		File cucumberFileDirectory = new File(cucumberFile);
+		if (cucumberFileDirectory.isDirectory()) {
+			try {
+				FileUtils.cleanDirectory(cucumberFileDirectory);
+				Thread.sleep(2000);
+				System.out.println("Previously generated Cucumber Reort Execution Files Deleted Permanently");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+
+	}
 	@AfterSuite
 	public void cucumberReportGenerate() {
 		Global.generateJVMReport(
